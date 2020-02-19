421d01471471d907da4dfd1b6792e3391390766b
Danielyan86 <516495459@qq.com>
2020-02-19 15:24:01 +0800

    update chapter1

--- chapter1/section1.7_named_arguments.sh
+++ chapter1/section1.7_named_arguments.sh
@@ -2,11 +2,13 @@
 deploy=false
 uglify=false
 
-while (( $# > 1 )); do case $1 in
-  --deploy) deploy="$2";;
-  --uglify) uglify="$2";;
-  *) break;
-esac; shift 2
+while (($# > 1)); do
+  case $1 in
+  --deploy) deploy="$2" ;;
+  --uglify) uglify="$2" ;;
+  *) break ;;
+  esac
+  shift 2
 done
 
 # ./script.sh --deploy true --uglify false
