34ae99c7d7b0a66cd0d17039844d4d797f7276eb
Danielyan86 <516495459@qq.com>
2020-02-27 15:49:37 +0800

    update sed

--- sed/sed.sh
+++ sed/sed.sh
@@ -1,6 +1,18 @@
+#!/usr/bin/env bash
 # replace the string in file directly
-sed -i 's/AA/75/g' test.file
+touch test.txt
+echo "AABBCC" >test.txt
+sed -i "" 's/AA/75/g' test.txt
+cat test.txt
 
-line="abc"
-sed 's/a/0/g' $line
-echo $line
+echo "***this is split line ***"
+touch test_2.txt
+echo "DDEEFF" >test_2.txt
+echo "XXYY" >>test_2.txt
+cat test_2.txt | sed 's/DD/88/g' > file_name_list
+echo "file name list"
+cat file_name_list
+
+while IFS= read -r file_name; do
+  echo $file_name
+done < file_name_list
