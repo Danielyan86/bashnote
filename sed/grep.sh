64f185aa1eff32f02499f0713555c849b4f9a0b8
Danielyan86 <516495459@qq.com>
2020-03-01 21:19:18 +0800

    update sed

--- sed/grep.sh
+++ sed/grep.sh
@@ -1,2 +1,7 @@
 #!/usr/bin/env bash
-grep -v "D\s" |
+# gerp is used for search string in file with regex pattern
+#grep [-abcEFGhHilLnqrsvVwxy][-A<显示列数>][-B<显示列数>][-C<显示列数>][-d<进行动作>]
+#[-e<范本样式>][-f<范本文件>][--help][范本样式][文件或目录...]
+
+# list the line which include the BB string
+grep BB test.txt
